6c18dc7ac1f32df8911af8c72ea8b1d8fef8caf5
praveen-kr2029 <praveen38072@gmail.com>
2026-05-05 19:48:45 +0530

    UC10: Detect draw condition when board is full and no winner

---
+++ DetectDraw.java
@@ -0,0 +1,41 @@
+class DetectDraw {
+
+    char[][] board = {
+        {'X', 'O', 'X'},
+        {'X', 'O', 'O'},
+        {'O', 'X', 'X'}
+    };
+
+    public boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean checkWin(char symbol) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) return true;
+            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) return true;
+        }
+
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) return true;
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) return true;
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        DetectDraw game = new DetectDraw();
+
+        if (!game.checkWin('X') && !game.checkWin('O') && game.isBoardFull()) {
+            System.out.println("Game is a Draw!");
+        } else {
+            System.out.println("Game is not a draw.");
+        }
+    }
+}
